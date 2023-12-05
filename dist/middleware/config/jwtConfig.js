a880a8592ddcd62d804735b79a107d678fc13b66
Muhammad Avicena <cenarahmant.dev@gmail.com>
2023-12-05 14:10:18 +0700

    chore: remove dist folder

--- dist/middleware/config/jwtConfig.js
+++
@@ -1,4 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.JWT_SIGN = void 0;
-exports.JWT_SIGN = process.env.JWT_SIGN;
--- dist/middleware/cspPolicyMiddleware.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-function cspPolicyMiddleware(app) {
-    app.use((req, res, next) => {
-        res.setHeader("Content-Security-Policy", "script-src 'self' https://folklof.com");
-        next();
-    });
-}
-exports.default = cspPolicyMiddleware;
