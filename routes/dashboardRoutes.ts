2ec9dfc1cc1d6d84dddf2086b233fdd33909ddfe
Muhammad Avicena <cenarahmant.dev@gmail.com>
2023-12-12 05:07:20 +0700

    [HAN-59] fix: error dashboard access admin

--- routes/dashboardRoutes.ts
+++ routes/dashboardRoutes.ts
@@ -4,13 +4,12 @@ import {
 } from "../controller/dashboardController";
 import { Router } from "express";
 import {
-  adminAuthorization,
   isAuthenticatedGoogle,
 } from "../middleware/authMiddleware";
 
 const router = Router();
 
-router.get("/admin", adminAuthorization, dashboardAdminAccess);
+router.get("/admin", isAuthenticatedGoogle, dashboardAdminAccess);
 router.get("/user", isAuthenticatedGoogle, dashboardUserAccess);
 
 export default router;
